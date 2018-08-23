6e2b9276bd04e5ac46e4e45639148cb41438d915
tuckersjomeling <tuckersjomeling@users.noreply.github.com>
2018-08-23 08:53:58 -0500

    hack001 first project

---
+++ Hack001.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Tucker Sjomeling
+ * Date: 2018/08/23
+ *
+ * Hack 01
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tucker Sjomeling");
+
+  return 0;
+}
